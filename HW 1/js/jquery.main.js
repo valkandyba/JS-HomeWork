acf7672f8521b438138ad143267f10fd007492bd
ValentinkaV <31902735+ValentinkaV@users.noreply.github.com>
2017-09-13 23:14:26 +0300

    change js

--- HW 1/js/jquery.main.js
+++ HW 1/js/jquery.main.js
@@ -1,8 +1,7 @@
 "use strict";
 
-var array = [4, 2, 7, 5, 9, -3, 10, 15, 45, 83, 128, -15, 95];
+var array = [4, 123, 2, 7, 5, 9, -3, 10, 15, 45, 83, 128, -15, 95];
 console.log("Original Array: " + array);
-console.log("Original Array: " + array.length);
 
 function swap(array, firstIndex, secondIndex){
 	const temp = array[firstIndex];
@@ -11,15 +10,15 @@ function swap(array, firstIndex, secondIndex){
 }
 
 function partition(array, left, right) {
-	var pivot   = array[Math.floor((right + left) / 2)],
-		i       = left,
-		j       = right;
+	var pointer = array[Math.floor((right + left) / 2)];
+	var i = left;
+	var j = right;
 
 	while (i <= j) {
-		while (array[i] < pivot) {
+		while (array[i] < pointer) {
 			i++;
 		}
-		while (array[j] > pivot) {
+		while (array[j] > pointer) {
 			j--;
 		}
 		if (i <= j) {
@@ -49,21 +48,3 @@ function quickSort(array, left, right) {
 
 var result = quickSort(array);
 console.log("Sorted Array: " + result);
-
-
-// function quickSort(array, left, right) {
-// 	var index;
-// 	if (array.length > 1) {
-// 		index = partition(array, left, right);
-// 		if (left < index - 1) {
-// 			quickSort(array, left, index - 1);
-// 		}
-// 		if (index < right) {
-// 			quickSort(array, index, right);
-// 		}
-// 	}
-// 	return array;
-// }
-
-// var result = quickSort(array, 0, array.length - 1);
-// console.log("Sorted Array: " + result);
